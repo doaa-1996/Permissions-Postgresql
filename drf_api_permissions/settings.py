d19dd89d054e13c633bc9c098ba1d5e73f0c8d6a
doaa-1996 <doaaobeidat1996@gmail.com>
2021-07-28 19:43:23 +0300

    finish

--- drf_api_permissions/settings.py
+++ drf_api_permissions/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK={
 'DEFAULT_PERMISSION_CLASSES':[
-'rest_framework.permissions.AllowAny',
+'rest_framework.permissions.IsAuthenticated',
 ]
 }
 # Internationalization
